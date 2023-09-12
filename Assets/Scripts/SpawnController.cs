59ba4da70024b6cf4dc98862cd7e15d3f6568b59
Hamed <hamednourani1026@gmail.com>
2023-09-12 23:59:05 +0330

    Spawn new tiles if needed
    Add spawn controller script

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    [SerializeField] private GameObject _tilePrefab;
+    
+    
+    private void FixedUpdate()
+    {
+        CheckRaycast();
+    }
+
+    private void CheckRaycast()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f);
+        
+        if (hit.collider == null)
+        {
+            var newTile = Instantiate(_tilePrefab, transform.position, quaternion.identity);
+            
+            newTile.transform.position = Vector2.Lerp(newTile.transform.position,
+                new Vector2(transform.position.x, transform.position.y - 0.15f), 5f);
+        }
+    }
+}
